348cc723357e41b00b69c3bda2773f30e0d4ec8f
dkshtakin <shtakin55@gmail.com>
2021-12-18 19:02:18 +0300

    Fixed code style

--- simple.py
+++ simple.py
@@ -1,3 +1,4 @@
+"""Uznypa - telegram bot"""
 import os
 import telebot
 
@@ -7,19 +8,26 @@ bot = telebot.TeleBot(token, parse_mode=None)
 
 commands = ['help']
 
+
 @bot.message_handler(commands='start')
-def send_welcome(message):
+def send_greetings(message):
+    """Sends greetings to users"""
     bot.reply_to(message, "Привет!")
 
+
 @bot.message_handler(commands=commands[0])
-def send_welcome(message):
+def send_help_info(message):
+    """Sends list of available commands to users"""
     msg = 'Список доступных команд:\n'
-    for i in range(len(commands)):
-        msg += str(i + 1) + '. /' + commands[i] + '\n'
+    for i, cmd in enumerate(commands):
+        msg += str(i + 1) + '. /' + cmd + '\n'
     bot.send_message(message.chat.id, msg)
+
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
+    """Sends user's message back"""
     bot.send_message(message.chat.id, message.text)
+
 
 bot.infinity_polling()
